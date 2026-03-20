ee878f73c5b00b4d583ad4e2a9cb0c4bf12008b6
Mohammed Arshad <arshadmomin7750@gmail.com>
2026-03-20 20:21:32 +0530

    the csv has 300k+ entries not 3m+.

--- ds_task4.py
+++ ds_task4.py
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 df = pd.read_csv('Road Accident Data.csv')
 
 # Someone must have written 'Fetal' instead of 'Fatal'. Noticed it in 'Accident Severity Distribution Graph'.
-# The csv has more than 3m entries, Not sure how many times the error occured.
+# The csv has more than 300k entries, Not sure how many times the error occured.
 # Replacing 'Fetal' with 'Fatal', thus solving the issue.
 df['Accident_Severity'] = df['Accident_Severity'].replace('Fetal', 'Fatal')
 
